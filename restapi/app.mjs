d633d49aaeae09974dd2068b9b9785e45b1c707c
lahiru <lahirusammikaperera@gmail.com>
2024-04-27 05:15:15 -0700

    test57

--- restapi/app.mjs
+++ restapi/app.mjs
@@ -1,5 +1,12 @@
 import express from "express";
-const { cloudinary } = require('./cloudinary');
+
+const cloudinary = require('cloudinary').v2;
+cloudinary.config({
+    cloud_name: "dzfkeu1si",
+    api_key: "287171622571245",
+    api_secret:"qPQQtuGvH2_XsS048HNMD-tKg5g",
+});
+
 
 
 const app = express();
